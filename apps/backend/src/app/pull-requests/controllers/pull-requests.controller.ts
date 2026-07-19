ed948d1109684529b6793ed16864ebb586834f01
Zsolt Denes <denes.zsolt.work@gmail.com>
2026-07-19 21:28:01 +0300

    feat(DEV-201): DORA metrics calculations

--- apps/backend/src/app/pull-requests/controllers/pull-requests.controller.ts
+++ apps/backend/src/app/pull-requests/controllers/pull-requests.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { GithubSyncService } from '../services/github-sync.service';
 import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { PullRequestEntity } from '../entities/pull-request.enity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller('pull-requests') // Ez lesz az URL alapja: /api/pull-requests
 export class PullRequestsController {
   
   // Szintén Dependency Injection: A NestJS automatikusan átadja a Controllernek a Service-t
-  constructor(private readonly githubSyncService: GithubSyncService) {}
+  constructor(
+    private readonly githubSyncService: GithubSyncService,
+    @InjectRepository(PullRequestEntity)
+    private readonly prRepository: Repository<PullRequestEntity>,
+  ) {}
 
   // Ez a dekorátor mondja meg, hogy ez egy GET végpont lesz.
   // A végleges URL: GET /api/pull-requests/test-sync
@@ -23,5 +30,21 @@ export class PullRequestsController {
     
     // ...és egyszerűen visszaküldjük a hívónak (a böngészőnek).
     return data; 
+  }
+
+  // Egy sima GET kérés a /api/pull-requests végpontra
+  @Get()
+  async getAllPrs() {
+    // Lekérjük az összes PR-t az adatbázisból, a legújabbakkal kezdve
+    const prs = await this.prRepository.find({
+      order: {
+        openedAt: 'DESC', 
+      },
+    });
+
+    return {
+      count: prs.length,
+      data: prs,
+    };
   }
 }
