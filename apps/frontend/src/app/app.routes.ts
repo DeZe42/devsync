b492adfec57d7595486e90a53543fe87d63b3c9b
Zsolt Denes <denes.zsolt.work@gmail.com>
2026-08-26 10:14:09 +0300

    feat(DEV-301): create angular router and layout component

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
+import { Layout } from './layout/layout';
+import { Dashboard } from './dashboard/dashboard';
+import { Settings } from './settings/settings';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+    path: '',
+    component: Layout,
+    children: [
+      { path: 'dashboard', component: Dashboard },
+      { path: 'settings', component: Settings },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ],
+  },
+];
