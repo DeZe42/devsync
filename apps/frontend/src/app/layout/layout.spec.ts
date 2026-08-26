42c834be5a7d79cde006a26ce96a7b48a9a54206
Zsolt Denes <denes.zsolt.work@gmail.com>
2026-08-26 10:25:07 +0300

    feat(DEV-301): fix tests

--- apps/frontend/src/app/layout/layout.spec.ts
+++ apps/frontend/src/app/layout/layout.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Layout } from './layout';
+import { RouterModule } from '@angular/router';
 
 describe('Layout', () => {
   let component: Layout;
@@ -7,7 +8,7 @@ describe('Layout', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Layout]
+      imports: [Layout, RouterModule.forRoot([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Layout);
